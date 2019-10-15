a1aae363a813df23664d5dbcf16ec3eb494070ea
ncooney94 <nick.cooney@gmail.com>
2019-10-15 14:45:00 +1100

    added reset button

--- js/main.js
+++ js/main.js
@@ -12,18 +12,18 @@ $(document).on('click', '.square', function() {
       nought = nought += Number($(this).text());
       noughtScore.push(Number($(this).text()));
       winner(nought, noughtScore);
-      console.log("nought");
-      $(this).removeClass('square');
-      $(this).addClass('nought');
-      turn = "cross";
+
+        $(this).removeClass('square');
+        $(this).addClass('nought');
+        turn = "cross";
   } else {
       cross = cross += Number($(this).text());
       crossScore.push(Number($(this).text()))
       winner(cross, crossScore);
-      $(this).removeClass('square');
-      $(this).addClass('cross');
-      console.log("cross");
-      turn = "nought";
+
+        $(this).removeClass('square');
+        $(this).addClass('cross');
+        turn = "nought";
   }
 });
 
@@ -42,27 +42,26 @@ const winner = function(score, player) {
     //score needs to use only 3 selections
 
     if (score === winningScores[i]) {
-      alert(`winner`);
+      console.log('here');
+      console.log('winner');
+
     } else if( (score - player[0]) === winningScores[i]  ){
-      alert('winner');
+      // $('.nought, .cross').removeClass().addClass('square');
+      console.log('winner');
     } else if( (score - player[1]) === winningScores[i]  ){
-      alert('winner');
+      // $('.nought, .cross').removeClass().addClass('square');
+      console.log('winner');
     } else if( (score - player[2]) === winningScores[i]  ){
-      alert('winner');
+      // $('.nought, .cross').removeClass().addClass('square');
+      console.log('winner');
     } else if( (score - player[3]) === winningScores[i]  ){
-      alert('winner');
+      // $('.nought, .cross').removeClass().addClass('square');
+      console.log('winner');
     }
   }
   return false;
 };
 
-// const guess = function(score) {
-//   for (let i = 0; score.length > i; i++) {
-//     // console.log(score[i]);
-//     win = score[i]+score[i+1]+score[i+2];
-//     console.log(win);
-//   }
-// }
-// console.log(winner(146));
-//need to factor in an incorrect guess. can only accept 3 squares to reach a
-// correct total.
+$('#reset').click(function() {
+  $('.nought, .cross').removeClass().addClass('square');
+});
