0a48d4374dbb7c1d92d0068bec65a9a2ad0609c0
ncooney94 <nick.cooney@gmail.com>
2019-10-15 11:53:49 +1100

    trying to factor in 4 squares

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 // toggle between knots and crosses
 let turn = "knought";
-let knoughtScore = 0;
-let crossScore = 0;
+let knoughtScore = [];
+let crossScore = [];
+let win = 0
 const winningScores = [7, 56, 73, 84, 146, 273, 292, 448];
 
 $(document).on('click', '.square', function() {
 
     if (turn === "knought") {
-      knoughtScore = knoughtScore += Number($(this).text());
+      // knoughtScore = knoughtScore += Number($(this).text());
+      knoughtScore.push(Number($(this).text()));
       winner(knoughtScore);
       console.log("knought");
       $(this).removeClass('square');
@@ -16,8 +18,8 @@ $(document).on('click', '.square', function() {
       // let knot = $(this).html('<h5>knot</h5>');
       turn = "cross";
   } else {
-    console.log('cross');
-      crossScore = crossScore += Number($(this).text());
+      // crossScore = crossScore += Number($(this).text());
+      crossScore.push(Number($(this).text()))
       winner(crossScore);
       $(this).removeClass('square');
       $(this).addClass('cross');
@@ -32,11 +34,13 @@ $(document).on('click', '.square', function() {
 //      |64|128|256| 448
 //      73  146 292
 
-
+// const turns; //log how many turns are taken
 
 const winner = function(score) {
   for (let i = 0; winningScores.length > i; i++) {
     // console.log(winningScores[i]);
+    //score needs to use only 3 selections
+
     if (score === winningScores[i]) {
       // return 'winner';
       alert(`winner`);
@@ -44,6 +48,14 @@ const winner = function(score) {
   }
   return false;
 };
+
+const guess = function(score) {
+  for (let i = 0; score.length > i; i++) {
+    // console.log(score[i]);
+    win = score[0]+score[1]+score[2];
+    console.log(win);
+  }
+}
 // console.log(winner(146));
 //need to factor in an incorrect guess. can only accept 3 squares to reach a
 // correct total.
