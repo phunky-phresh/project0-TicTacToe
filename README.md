8e675e6fde9f3261c1e93214c579c3c8a920434c
ncooney94 <nick.cooney@gmail.com>
2019-10-17 23:15:58 +1100

    DRY, comments. indentation

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Tic, Tac, Toe - Project 0
 
-[Live Demo](https://phunky-phresh.github.io/project0-TicTacToe/)
+### Demo
+check out the [Live Demo](https://phunky-phresh.github.io/project0-TicTacToe/)
 
 ## Description
 This is project 0, Tic, Tac, Toe. For this project the requirements were to build a working Tic, Tac, Toe game from scratch.
 
-Project 0 a
+The requirements included functionality such as:
+- Build web app from scratch
+- Utilise Javascript, CSS and HTML files
+- Render a board game in the browser
+- Allow two players, switching between them
+- Declare winners or Draws.
 
 ## Features
  - Win and Draw displays: After a player wins, they are declared the winner. If a draw occurs, a Draw is declared.
@@ -29,8 +35,7 @@ const winningScores = [7, 56, 73, 84, 146, 273, 292, 448];
 ```
 My logic was based on collecting the scores of each square clicked by a player, then finding the sum of the selected squares.
 If the sum was equal to a winning value, a winner would be declared.
-## Screenshots
-![Alt text](../images/star.png "Optional title")
+
 ## Technology Used
 - jQuery 3.4.1
 - CSS3
@@ -41,8 +46,17 @@ If the sum was equal to a winning value, a winner would be declared.
 ## Known Bugs
 there is one bug present that can be fixed! If player two decides to throw the game and make no attempt to win, the game cannot verify a win of 5 moves by player one. Because all the squares have been counted, it considers this a draw.
 
-### Can it be done?
-for sure, once the code is more refined, it will hopefully require a lot less code to factor in this type of win. for now, just **try not to lose player two!**
+``` js
+let count = 0;
+
+const tracker = function() {
+  count += 1;
+
+  if (count === 9)
+```
+
+### Can it be fixed?
+For sure, once the code is more refined, it will hopefully require a lot less code to factor in this type of win. for now, just **try not to lose, player two!**
 ## Acknowledgement
 
 ## Wish List
@@ -50,7 +64,7 @@ for sure, once the code is more refined, it will hopefully require a lot less co
 - competitive AI
 - ability to pick characters before playing game
 
-### help text
+### Help text
 The game page loads with a quick tutorial on what Tic, Tac, Toe is and the aim of the game. Once the 'Play Game' button is clicked, player one will be able to select their first square. Player two will follow and the players will take turns till the game is won or drawn.
 
-when a draw or win is declared, players will have the option to continue playing and keep track of wins in their respective score board.
+When a draw or win is declared, players will have the option to continue playing and keep track of wins in their respective score board.
--- js/main.js
+++ js/main.js
@@ -19,14 +19,16 @@ let crossCount = 0;
 let winner = ''
 const winningScores = [7, 56, 73, 84, 146, 273, 292, 448];
 
-//Click Function that switches between players
+// Click Function that switches between players
 // also executes other functions
 $(document).on('click', '.square', function() {
 
     if (turn === "nought") {
       noughtCount += Number($(this).text());
       noughtArray.push(Number($(this).text()));
+      //player declared to keep track of turns
       player = 'one'
+      //functions run every click turn
       countCheck(noughtCount, noughtArray, player);
       tracker();
       scoreTracker();
@@ -48,7 +50,8 @@ $(document).on('click', '.square', function() {
   }
 });
 
-// WIN function
+///// WIN function /////
+// the countCheck runs through the noughtArray and crossArray to check with potential winner scores.
 const countCheck = function(count, array, player) {
 
   for (let i = 0; winningScores.length > i; i++) {
@@ -57,7 +60,7 @@ const countCheck = function(count, array, player) {
     if (count === winningScores[i]) {
       winner = player;
       winGame();
-
+      // these if else statements run to remove the 4th square selected and check for a winning score.
     } else if( (count - array[0]) === winningScores[i] ){
       winner = player;
       winGame();
@@ -83,7 +86,8 @@ const countCheck = function(count, array, player) {
 };
 
 /// WINNER display ///
-
+// this function runs when a player wins. it produces a drop down window in the DOM with a play again option.
+//called in the top click function
 const winGame = function() {
   $('header').after('<div class="animated bounceInDown endGame"></div>')
   $('.endGame').append(`<h2 class="end">Player: ${player} Wins!</h2><button type="button" name="reset" class="reset">Play Again?</button>`);
@@ -91,8 +95,10 @@ const winGame = function() {
 }
 
 /// DRAW function ///
+// checks if all squares have been clicked without a winner.
+//called in top click function
 let count = 0;
-//
+
 const tracker = function() {
   count += 1;
 
@@ -103,6 +109,7 @@ const tracker = function() {
 };
 
 /// SCORE keeper ////
+// function to update score board
 const scoreTracker = function() {
   let currentScore = Number($(`#${winner}`).html());
   currentScore += 1;
@@ -113,6 +120,8 @@ const scoreTracker = function() {
 // once players reach certain scores, characters level up/evolve
 
 // GAME RESET //
+// reset button that returns game board to starting point.
+//does not remove active score board counts
 $(document).on('click', '.reset', function() {
   $('.nought, .cross').removeClass().addClass('square');
   $('#winner').css('visibility', 'hidden');
@@ -135,11 +144,11 @@ $(document).on('click', '.reset', function() {
   winner = '';
 });
 
-// removes Help page //
+// removes Tutorial page //
 $(document).on('click', '.start', function() {
   $('.help').addClass('animated fadeOutDown');
   setTimeout(function(){
   $('.help').remove();
 }, 1000);
-  // $('.help').hide();
-})
+
+});
